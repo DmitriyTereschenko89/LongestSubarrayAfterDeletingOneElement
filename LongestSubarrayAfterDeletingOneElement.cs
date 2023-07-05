3cb67f43c3f87f738a49615e7c0e397d59babc71
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-07-05 16:09:10 +0300

    Добавьте файлы проекта.

---
+++ LongestSubarrayAfterDeletingOneElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubarrayAfterDeletingOneElement
+{
+	internal class LongestSubarrayAfterDeletingOneElement
+	{
+		public int LongestSubarray(int[] nums)
+		{
+
+		}
+	}
+}
