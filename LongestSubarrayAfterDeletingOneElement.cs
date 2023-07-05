dc0f1669c55e407ed2592b9b78250c7ddf2f8fee
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-07-05 16:14:44 +0300

    update read.me add solution

--- LongestSubarrayAfterDeletingOneElement.cs
+++ LongestSubarrayAfterDeletingOneElement.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LongestSubarrayAfterDeletingOneElement
+﻿namespace LongestSubarrayAfterDeletingOneElement
 {
 	internal class LongestSubarrayAfterDeletingOneElement
 	{
 		public int LongestSubarray(int[] nums)
 		{
-
+			int zeroCount = 0;
+			int leftBound = 0;
+			int longestSubarray = 0;
+			int n = nums.Length;
+			for (int rightBound = 0; rightBound < n; ++rightBound)
+			{
+				zeroCount += (nums[rightBound] == 0) ? 1 : 0;
+				while(zeroCount > 1)
+				{
+					zeroCount -= (nums[leftBound] == 0) ? 1 : 0;
+					++leftBound;
+				}
+				longestSubarray = Math.Max(longestSubarray, rightBound - leftBound);
+			}
+			return longestSubarray;
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			LongestSubarrayAfterDeletingOneElement longestSubarrayAfterDeletingOneElement = new();
+            Console.WriteLine(longestSubarrayAfterDeletingOneElement.LongestSubarray(new int[] { 1, 1, 0, 1 }));
+			Console.WriteLine(longestSubarrayAfterDeletingOneElement.LongestSubarray(new int[] { 0, 1, 1, 1, 0, 1, 1, 0, 1 }));
+			Console.WriteLine(longestSubarrayAfterDeletingOneElement.LongestSubarray(new int[] { 1, 1, 1 }));
 		}
 	}
 }
